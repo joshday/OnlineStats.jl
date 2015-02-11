4f072983ce32bf26f75b91c7355e1e6cf9be1b7f
Josh Day <jtday2@ncsu.edu>
2015-02-10 21:29:34 -0500

    use abstract type hierarchy from StreamStats.jl

---
+++ src/onlinestat.jl
@@ -0,0 +1,24 @@
+abstract OnlineStat{
+    F <: Distributions.VariateForm,
+    S <: Distributions.ValueSupport
+}
+
+typealias DiscreteUnivariateOnlineStat OnlineStat{
+    Distributions.Univariate,
+    Distributions.Discrete
+}
+
+typealias ContinuousUnivariateOnlineStat OnlineStat{
+    Distributions.Univariate,
+    Distributions.Continuous
+}
+
+typealias DiscreteMultivariateOnlineStat OnlineStat{
+    Distributions.Multivariate,
+    Distributions.Discrete
+}
+
+typealias ContinuousMultivariateOnlineStat OnlineStat{
+    Distributions.Multivariate,
+    Distributions.Continuous
+}
