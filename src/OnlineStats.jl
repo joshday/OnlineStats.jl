d928af24b05387d67b1c91b9d4067142d818f443
Josh Day <jtday2@ncsu.edu>
2015-02-12 14:00:30 -0500

    add n_obs and n_batches functions

--- src/OnlineStats.jl
+++ src/OnlineStats.jl
@@ -5,18 +5,31 @@ using Docile
 using Distributions
 import Base.merge
 
-export update!, state, onlinefit
+export update!, state, onlinefit, n_obs, n_used
 
 
-
+# Abstract Type structure
 include("onlinestat.jl")
 
+# Summary Statistics
 include("summary/summary.jl")
 include("summary/quantile.jl")
 include("summary/moments.jl")
 
+# Density Estimation
 include("densityestimation/normal.jl")
 include("densityestimation/binomial.jl")
+include("densityestimation/bernoulli.jl")
+
+
+# General functions
+function n_obs(obj)
+   obj.n
+end
+
+function n_batches(obj)
+   obj.nb
+end
 
 
 # General docs for update!, state, convert
