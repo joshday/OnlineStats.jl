14093c371faf6e254b18aa6e86ea71c1ed78c57f
Josh Day <emailjoshday@gmail.com>
2015-02-23 12:22:21 -0500

    rename LinearModel to OnlineLinearModel

--- src/linearmodel/lm.jl
+++ src/linearmodel/lm.jl
@@ -1,37 +1,37 @@
 # Author(s): Josh Day <emailjoshday@gmail.com>
 
-export LinearModel, coef, cov
+export OnlineLinearModel, coef, cov
 
 
 #-----------------------------------------------------------------------------#
 #---------------------------------------------------------------------# NewType
-type LinearModel <: OnlineStat
+type OnlineLinearModel <: OnlineStat
     A::Matrix              # [X y]' * [X y] / n (lower triangular)
     B::Matrix              # swept A (lower triangular)
     n::Int64
     nb::Int64
 end
 
-function LinearModel(X::Matrix, y::Vector)
+function OnlineLinearModel(X::Matrix, y::Vector)
     size(X, 1) == length(y) || error("rows in X don't match length of y")
     n, p = size(X)
     A = BLAS.syrk('L', 'T', 1.0, [X y])
     B = sweep!(copy(A), 1:p)
-    LinearModel(A, B, n, 1)
+    OnlineLinearModel(A, B, n, 1)
 end
 
-function LinearModel(x::Vector, y::Vector)
+function OnlineLinearModel(x::Vector, y::Vector)
     n = length(x)
-    LinearModel(reshape(x, n, 1), y)
+    OnlineLinearModel(reshape(x, n, 1), y)
 end
 
-coef(obj::LinearModel) = obj.B[end, 1:end-1]'
-mse(obj::LinearModel) = obj.B[end, end] / (obj.n - size(obj.A, 1))
+coef(obj::OnlineLinearModel) = obj.B[end, 1:end-1]'
+mse(obj::OnlineLinearModel) = obj.B[end, end] / (obj.n - size(obj.A, 1))
 
 
 #-----------------------------------------------------------------------------#
 #---------------------------------------------------------------------# update!
-function update!(obj::LinearModel, X, y)
+function update!(obj::OnlineLinearModel, X, y)
     n, p = size(X)
     n == length(y) || error("size(X, 1) != length(y)")
     p == size(obj.A, 2) - 1 || error("number of variables don't match")
@@ -45,7 +45,7 @@ end
 
 #-----------------------------------------------------------------------------#
 #-----------------------------------------------------------------------# state
-function state(obj::LinearModel)
+function state(obj::OnlineLinearModel)
     p = size(obj.A, 1) - 1
     names = [[symbol("b" * string(i)) for i in 1:p], :n, :nb]
     estimates = [coef(obj), obj.n, obj.nb]
@@ -57,7 +57,7 @@ end
 # Batch 1
 x1 = randn(1000, 3)
 y1 = vec(sum(x1, 2)) + randn(1000)
-obj = OnlineStats.LinearModel(x1, y1)
+obj = OnlineStats.OnlineLinearModel(x1, y1)
 
 # Batch 2
 x2 = rand(1002, 3)
