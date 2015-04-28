4ebf048bd2ebdb99b82d207239f4293b4e41de4d
Josh Day <emailjoshday@gmail.com>
2015-04-28 09:18:07 -0400

    clean up plotmethods.jl

--- plotmethods.jl
+++ plotmethods.jl
@@ -2,30 +2,6 @@
 # are the Gadfly.plot methods that I've done.
 
 #----------------------------------------------# Boxplot from FiveNumberSummary
-
-function Gadfly.plot(obj::MixtureModel{Univariate, Continuous, Normal}, a, b;
-                     args...)
-    plotvec = [x -> pdf(obj, x)]
-    legendvec = ["Mixture"]
-
-    for j in 1:length(components(obj))
-        plotvec = [plotvec; x -> probs(obj)[j] * pdf(components(obj)[j], x)]
-        legendvec = [legendvec; ["Component $j"]]
-    end
-
-    Gadfly.plot(plotvec, a, b, color = repeat(legendvec), args...)
-end
-
-function Gadfly.plot(obj::MixtureModel{Univariate, Continuous, Normal}, x;
-                     args...)
-    a = maximum(x)
-    b = minimum(x)
-    xvals = a:(b-a)/1000:b
-    yvals = pdf(obj, xvals)
-    Gadfly.plot(Gadfly.layer(x = xvals, y=yvals, Gadfly.Geom.line, order = 1,
-        Gadfly.Theme(default_color = Gadfly.color("black"))),
-        Gadfly.layer(x = x, Gadfly.Geom.histogram(density = true), order = 0))
-end
 function Gadfly.plot(obj::OnlineStats.FiveNumberSummary)
     s = OnlineStats.state(obj)[:value]
     iqr = s[3] - s[1]
@@ -55,6 +31,8 @@ function Gadfly.plot(obj::MixtureModel{Univariate, Continuous, Normal}, a, b;
     Gadfly.plot(plotvec, a, b, color = repeat(legendvec), args...)
 end
 
+
+#----------------------------------------# Normal Mixture overlaid on histogram
 function Gadfly.plot(obj::MixtureModel{Univariate, Continuous, Normal}, x;
                      args...)
     a = maximum(x)
