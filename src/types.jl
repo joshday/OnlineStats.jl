b9a6051a029d582fe2005e8644c166afdab7680f
Josh Day <emailjoshday@gmail.com>
2015-04-29 11:48:31 -0400

    Gamma now uses Weighting

--- src/types.jl
+++ src/types.jl
@@ -4,5 +4,3 @@ abstract OnlineStat
 # Possibly multiple parameters that are all scalars
 abstract ScalarStat <: OnlineStat
 
-# Estimation of parametric distribution
-abstract DistributionStat <: OnlineStat
