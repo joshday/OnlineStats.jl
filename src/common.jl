5c0e1bf9feb3151fb99417a48c6a6b8a54dc3429
Josh Day <emailjoshday@gmail.com>
2015-04-30 09:54:44 -0400

    Removing overcomplicated abstract types.  Back to only OnlineStat.

--- src/common.jl
+++ src/common.jl
@@ -21,6 +21,7 @@ mystring(f::Float64) = @sprintf("%f", f)
 mystring(x) = string(x)
 
 
+
 function Base.show(io::IO, o::NonMatrixStat)
     snames = statenames(o)
     svals = state(o)
@@ -38,10 +39,10 @@ end
 # Why doesn't this work in 0.3.7?
 # DataFrame(o::OnlineStat) = DataFrame(state(o), statenames(o))
 
-function DataFrame(o::NonMatrixStat)
+function DataFrame(o::OnlineStat)
     df = convert(DataFrame, state(o)')
     names!(df, statenames(o))
 end
 
-Base.push!(df::DataFrame, o::NonMatrixStat) = push!(df, state(o))
+Base.push!(df::DataFrame, o::OnlineStat) = push!(df, state(o))
 
