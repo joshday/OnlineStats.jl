c033b934a0d934eaf7913b7519d4e1f29308a71c
Josh Day <emailjoshday@gmail.com>
2015-05-01 16:30:39 -0400

    convert Mean tests to FactCheck

--- test/mean_test.jl
+++ test/mean_test.jl
@@ -20,34 +20,34 @@ facts("Mean") do
     @fact obj.μ => roughly(mean(x1))
     @fact obj.n => n1
 
-#     update!(obj, x2)
-#     @fact_approx_eq obj.μ  mean(x)
-#     @fact obj.n => n
+    update!(obj, x2)
+    @fact obj.μ => roughly(mean(x))
+    @fact obj.n => n
 
-#     obj1 = Mean(x1)
-#     obj2 = Mean(x2)
-#     obj3 = merge(obj1, obj2)
-#     merge!(obj1, obj2)
-#     @fact obj1.n => obj3.n
-#     @fact_approx_eq obj1.μ obj3.μ
-#     @fact_approx_eq mean(x) mean(obj1)
+    obj1 = Mean(x1)
+    obj2 = Mean(x2)
+    obj3 = merge(obj1, obj2)
+    merge!(obj1, obj2)
+    @fact obj1.n => obj3.n
+    @fact obj1.μ => roughly(obj3.μ)
+    @fact mean(x)=> roughly(mean(obj1))
 
 
-#     # empty constructor, state, Base.mean, nobs, Base.copy
-#     obj = Mean()
-#     @fact obj.μ => 0.0
-#     @fact obj.n => 0
-#     # @fact state(obj, DataFrame) => DataFrame(variable = :μ, value = 0., nobs=0)
-#     @fact mean(obj) => 0.0
-#     update!(obj, x1)
-#     @fact_approx_eq mean(obj) mean(x1)
-#     @fact nobs(obj) => n1
-#     obj1 = copy(obj)
-#     @fact_approx_eq mean(obj) mean(x1)
-#     @fact nobs(obj) => n1
-#     obj2 = Mean(x1[1])
-#     @fact mean(obj2) => x1[1]
-#     @fact nobs(obj2) => 1
+    # empty constructor, state, Base.mean, nobs, Base.copy
+    obj = Mean()
+    @fact obj.μ => 0.0
+    @fact obj.n => 0
+    # @fact state(obj, DataFrame) => DataFrame(variable = :μ, value = 0., nobs=0)
+    @fact mean(obj) => 0.0
+    update!(obj, x1)
+    @fact mean(obj) => roughly(mean(x1))
+    @fact nobs(obj) => n1
+    obj1 = copy(obj)
+    @fact mean(obj) => roughly(mean(x1))
+    @fact nobs(obj) => n1
+    obj2 = Mean(x1[1])
+    @fact mean(obj2) => x1[1]
+    @fact nobs(obj2) => 1
 
-end
-end
+end # facts
+end # module
