ba5a24b14535f43415ba7ac85a3369d3375927cb
Thomas Breloff <tom@breloff.com>
2015-05-04 17:10:16 -0400

    working on opca

--- src/linearmodel/opca.jl
+++ src/linearmodel/opca.jl
@@ -1,21 +1,15 @@
 
-# OLD: implementation similar to: https://github.com/kevinhughes27/pyIPCA/blob/master/pyIPCA/ccipca.py
-
-# Based on Boutsidis et al: "Online Principal Component Analysis"
 
 type OnlinePCA{W<:Weighting} <: OnlineStat
-		L::MatF  # pca loading matrix
-		d::Int  # number of input vars
-    k::Int  # number of principal components
-    n::Int
-    weighting::W
 
-    # needed for update
-    e::Float64 	# ???
-    U::MatF   	# d x k/e^3... init to zeros
-    Z::MatF			# d x k/e^2... init to zeros
-    w::Float64	#          ... init to 0.0
-    wu::VecF 		# k/e^3 x 1... init to zeros
+
+
+	L::MatF  # pca loading matrix
+	d::Int  # number of input vars
+  k::Int  # number of principal components
+  n::Int
+  weighting::W
+
 end
 
 
@@ -36,37 +30,68 @@ function getFirstEig(A::MatF)
 	λ, v
 end
 
-
 function update!(o::OnlinePCA, x::VecF)
-	update_algo1!(o, x)
-	# update_algo2!(o, x)
-end
 
-
-function update_algo1!(o::OnlinePCA, x::VecF)
-	norm2x = ???  # TODO some constant? how to define?
-	l = ceil(8k / e^2)
-	ImUU = eye(d) - U * U'  		# d x d
-	r = ImUU * x 								# d x 1
-	C = ImUU * (Z * Z') * ImUU 	# d x d
-
-	# fill in for each component
-	while norm(C + r * r') >= max(w0, w) * k / e^2
-		λ, u = getFirstEig(C)
+	for i in 1:o.k
 	end
-
 end
 
-function update_algo2!(o::OnlinePCA, x::VecF)
-	w0 = 0.0  # do we ever need to change this??
-	w += sumabs2(x)
-	ImUU = eye(d) - U * U'  		# d x d
-	r = ImUU * x 								# d x 1
-	C = ImUU * (Z * Z') * ImUU 	# d x d
 
-	# fill in for each component
-	while norm(C + r * r') >= max(w0, w) * k / e^2
-		λ, u = getFirstEig(C)
-	end
+# TODO: roughly based on Weng et al (2003): "Candid covariance-free incremental principal component analysis"
+# used https://github.com/kevinhughes27/pyIPCA/blob/master/pyIPCA/ccipca.py as a reference
 
-end
+
+
+
+# ------------------------------------------------------------ unused
+
+# type OnlinePCA{W<:Weighting} <: OnlineStat
+# 		L::MatF  # pca loading matrix
+# 		d::Int  # number of input vars
+#     k::Int  # number of principal components
+#     n::Int
+#     weighting::W
+
+#     # needed for update
+#     e::Float64 	# ???
+#     U::MatF   	# Projection matrix -- d x k/e^3... init to zeros
+#     Z::MatF			# d x k/e^2... init to zeros
+#     w::Float64	#          ... init to 0.0
+#     wu::VecF 		# k/e^3 x 1... init to zeros
+# end
+
+# function update!(o::OnlinePCA, x::VecF)
+# 	update_algo1!(o, x)
+# 	# update_algo2!(o, x)
+# end
+
+# # TODO: Based on Boutsidis et al: "Online Principal Component Analysis" - Algo #1
+# function update_algo1!(o::OnlinePCA, x::VecF)
+# 	norm2x = ???  # TODO some constant? how to define?
+# 	l = ceil(8k / e^2)
+# 	ImUU = eye(d) - U * U'  		# d x d
+# 	r = ImUU * x 								# d x 1
+# 	C = ImUU * (Z * Z') * ImUU 	# d x d -- covariance of residual errors
+
+# 	# fill in for each component
+# 	while norm(C + r * r') >= max(w0, w) * k / e^2
+# 		λ, u = getFirstEig(C)
+# 	end
+
+# end
+
+
+# # TODO: Based on Boutsidis et al: "Online Principal Component Analysis" - Algo #2
+# function update_algo2!(o::OnlinePCA, x::VecF)
+# 	w0 = 0.0  # do we ever need to change this??
+# 	w += sumabs2(x)
+# 	ImUU = eye(d) - U * U'  		# d x d
+# 	r = ImUU * x 								# d x 1
+# 	C = ImUU * (Z * Z') * ImUU 	# d x d
+
+# 	# fill in for each component
+# 	while norm(C + r * r') >= max(w0, w) * k / e^2
+# 		λ, u = getFirstEig(C)
+# 	end
+
+# end
