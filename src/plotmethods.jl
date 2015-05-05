ac004838778e9c89fba82a2b42d23ffa518f3241
Josh Day <emailjoshday@gmail.com>
2015-05-05 08:46:21 -0400

    move plotmethods.jl into src

--- src/plotmethods.jl
+++ src/plotmethods.jl
@@ -2,19 +2,19 @@
 # are the Gadfly.plot methods that I've done.
 
 #----------------------------------------------# Boxplot from FiveNumberSummary
-function Gadfly.plot(obj::OnlineStats.FiveNumberSummary)
-    s = OnlineStats.state(obj)[:value]
-    iqr = s[3] - s[1]
-    Gadfly.plot(
-        Gadfly.layer(lower_fence = [maximum((s[2] - 1.5 * iqr, s[1]))],
-              lower_hinge = [s[2]],
-              middle = [s[3]],
-              upper_hinge = [s[4]],
-              upper_fence = [minimum((s[4] + 1.5 * iqr, s[5]))],
-              # outliers = [s[1], s[5]],
-              x = ["Data"], Gadfly.Geom.boxplot),
-        Gadfly.layer(x = ["Data"], y=[s[1], s[5]], Gadfly.Geom.point))
-end
+# function Gadfly.plot(obj::OnlineStats.FiveNumberSummary)
+#     s = OnlineStats.state(obj)[:value]
+#     iqr = s[3] - s[1]
+#     Gadfly.plot(
+#         Gadfly.layer(lower_fence = [maximum((s[2] - 1.5 * iqr, s[1]))],
+#               lower_hinge = [s[2]],
+#               middle = [s[3]],
+#               upper_hinge = [s[4]],
+#               upper_fence = [minimum((s[4] + 1.5 * iqr, s[5]))],
+#               # outliers = [s[1], s[5]],
+#               x = ["Data"], Gadfly.Geom.boxplot),
+#         Gadfly.layer(x = ["Data"], y=[s[1], s[5]], Gadfly.Geom.point))
+# end
 
 
 #--------------------------------------------------------------# Normal Mixture
