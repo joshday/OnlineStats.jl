1de9ed2767ccf32e754c47dbe8ce8e21ec3f4d6a
Josh Day <emailjoshday@gmail.com>
2015-05-10 09:25:19 -0400

    forgot to actually add analyticalpca.jl

---
+++ src/multivariate/analyticalpca.jl
@@ -0,0 +1,41 @@
+# This is a "slow" analytical update based on CovarianceMatrix
+# At each batch, CovarianceMatrix is updated and then eigenvalue decomposition is done.
+#-------------------------------------------------------# Type and Constructors
+type AnalyticalPCA{W <: Weighting} <: OnlineStat
+    C::CovarianceMatrix{W}
+    corr::Bool  # Use correlation instead of covariance?
+
+    # eigen:
+    values::VecF
+    vectors::MatF
+
+    n::Int64
+end
+
+
+AnalyticalPCA(p::Int, wgt::Weighting = default(Weighting), corr = true) =
+    AnalyticalPCA(CovarianceMatrix(p, wgt), corr, zeros(p), zeros(p, p), 0)
+
+function AnalyticalPCA(X::MatF, wgt::Weighting = default(Weighting), corr = true)
+    o = AnalyticalPCA(size(X, 2), wgt, corr)
+    updatebatch!(o, X)
+    o
+end
+
+
+#-----------------------------------------------------------------------# state
+statenames(o::AnalyticalPCA) = [:v, :λ, :nobs]  # decomposition is Av = λv
+state(o::AnalyticalPCA) = Any[o.vectors, o.values, o.n]
+
+
+#---------------------------------------------------------------------# update!
+function updatebatch!(o::AnalyticalPCA, X::MatF)
+    updatebatch!(o.C, X)
+    if o.corr
+        o.values, o.vectors = eig(cor(o.C))
+    else
+        o.values, o.vectors = eig(cov(o.C))
+    end
+    o.n += 1
+end
+
