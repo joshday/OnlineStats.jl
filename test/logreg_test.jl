7b4cfe9d93b557040447089f8502cf9781ed485b
Josh Day <emailjoshday@gmail.com>
2015-06-11 18:42:54 -0400

    testing logreg more

---
+++ test/logreg_test.jl
@@ -0,0 +1,63 @@
+module LogReg_test
+
+using OnlineStats, FactCheck, StatsBase
+
+logitinverse(x) = 1. / (1 + exp(-x))
+@vectorize_1arg Number logitinverse
+
+facts("LogReg") do
+    context("LogRegSGD") do
+        o = LogRegSGD(5)
+
+        β = [1:5]
+        x = randn(100, 5)
+        y = int(logitinverse(x*β) .< rand(100))
+
+        updatebatch!(o, x, y)
+        o = LogRegSGD(x, y)
+        updatebatch!(o, x, y)
+
+        @fact statenames(o) => [:β, :nobs]
+        @fact state(o)[1] => coef(o)
+        @fact state(o)[2] => nobs(o)
+
+        @fact predict(o, x) => logitinverse(x*coef(o))
+    end
+
+    context("LogRegSGD2") do
+        o = LogRegSGD2(5)
+
+        β = [1:5]
+        x = randn(100, 5)
+        y = int(logitinverse(x*β) .< rand(100))
+
+        updatebatch!(o, x, y)
+        o = LogRegSGD2(x, y)
+        updatebatch!(o, x, y)
+
+        @fact statenames(o) => [:β, :nobs]
+        @fact state(o)[1] => coef(o)
+        @fact state(o)[2] => nobs(o)
+
+        @fact predict(o, x) => logitinverse(x*coef(o))
+    end
+
+    context("LogRegMM") do
+        o = LogRegMM(5)
+
+        β = [1:5]
+        x = randn(100, 5)
+        y = int(logitinverse(x*β) .< rand(100))
+
+        updatebatch!(o, x, y)
+        o = LogRegMM(x, y)
+        update!(o, x, y)
+
+        @fact statenames(o) => [:β, :nobs]
+        @fact state(o)[1] => coef(o)
+        @fact state(o)[2] => nobs(o)
+
+        @fact predict(o, x) => logitinverse(x*coef(o))
+    end
+end
+end  # module
