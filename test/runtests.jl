5f90ad3b4f0d65b95b46e5260ad85b59552c80ae
Josh Day <emailjoshday@gmail.com>
2015-06-13 11:56:28 -0400

    testing. Something was causing travis build to fail.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,28 @@
 module OnlineStatsTests
 using FactCheck
 
-# include("common_test.jl")
-# include("mean_test.jl")
-# include("var_test.jl")
-# include("summary_test.jl")
-# include("extrema_test.jl")
-# include("fivenumber_test.jl")
+include("common_test.jl")
+include("mean_test.jl")
+include("var_test.jl")
+include("summary_test.jl")
+include("extrema_test.jl")
+include("fivenumber_test.jl")
 
-# include("quantiles_test.jl")
+include("quantiles_test.jl")
 
-# include("linearmodel_test.jl")
-# include("sweep_test.jl")
-# include("quantreg_test.jl")
-# include("logreg_test.jl")
+include("linearmodel_test.jl")
+include("sweep_test.jl")
+include("quantreg_test.jl")
+include("logreg_test.jl")
 
-# include("distribution_test.jl")
-# include("normalmix_test.jl")
+include("distribution_test.jl")
+include("normalmix_test.jl")
 
-# include("covmatrix_test.jl")
+include("covmatrix_test.jl")
 include("analyticalpca_test.jl")
 
-# include("plotmethods_test.jl")
-# include("tracedata_test.jl")
+include("plotmethods_test.jl")
+include("tracedata_test.jl")
 
 FactCheck.exitstatus()
 end # module
