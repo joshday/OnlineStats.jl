faafa47938a39780dd0ad7ae1df15eb59f052095
Josh Day <emailjoshday@gmail.com>
2015-10-14 16:54:13 -0400

    remove redefinitions for adding penalty for sgd

--- src/stochasticmodel/algorithms/sgdsparse.jl
+++ src/stochasticmodel/algorithms/sgdsparse.jl
@@ -48,18 +48,3 @@ Base.show(io::IO, o::SGDSparse) = print(io, "SGDSparse with rate γ_t = $(o.η) 
     end
     nothing
 end
-
-# For adding penalties
-@inline add∇j(x::Float64, ::NoPenalty, β::VecF, i::Int) = x
-@inline add∇j(x::Float64, p::L2Penalty, β::VecF, i::Int) = x + p.λ * β[i]
-@inline add∇j(x::Float64, p::L1Penalty, β::VecF, i::Int) = x + p.λ * sign(β[i])
-@inline add∇j(x::Float64, p::ElasticNetPenalty, β::VecF, i::Int) = x + p.λ * (p.α * sign(β[i]) + (1 - p.α) * β[i])
-@inline function add∇j(x::Float64, p::SCADPenalty, β::VecF, i::Int)
-    if abs(β[i]) < p.λ
-        x + sign(β[i]) * p.λ
-    elseif abs(β[i]) < p.a * p.λ
-        x + max(p.a * p.λ - abs(β[i]), 0.0) / (p.a - 1.0)
-    else
-        x
-    end
-end
