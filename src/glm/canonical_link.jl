3b8099dd8a9772c1d2d5f6498b46dd0a09f0cdf1
Josh Day <emailjoshday@gmail.com>
2015-10-21 14:43:53 -0400

    GLM works for normal, bernoulli, and poisson!

--- src/glm/canonical_link.jl
+++ src/glm/canonical_link.jl
@@ -1,7 +1,7 @@
 # MM gradient algorithm for GLMs with canonical link
 const _supported_dists = [
     Distributions.Normal,
-    Distributions.Binomial,
+    Distributions.Bernoulli,
     Distributions.Poisson
 ]
 
@@ -32,11 +32,20 @@ end
 
 function update!(o::GLM{Distributions.Poisson}, x::AVec, y::Float64, γ::Float64 = weight(o))
     ŷ = predict(o, x)
-    u = γ * (y - ŷ)
-    v =  1. / (sumabs(x) * ŷ)
+    u = γ * (y - ŷ) / (sumabs2(x) * ŷ)
 
     for j in 1:length(x)
-        o.β[j] += sign(x[j]) * u * v
+        o.β[j] += x[j] * u
+    end
+    o.n += 1
+end
+
+function update!(o::GLM{Distributions.Bernoulli}, x::AVec, y::Float64, γ::Float64 = weight(o))
+    ŷ = predict(o, x)
+    u = γ * (y - ŷ) / (sumabs2(x) * ŷ * (1 - ŷ))
+
+    for j in 1:length(x)
+        o.β[j] += x[j] * u
     end
     o.n += 1
 end
@@ -58,34 +67,47 @@ StatsBase.coef(o::GLM) = copy(o.β)
 StatsBase.predict(o::GLM, x::AMatF) = [predict(o,rowvec_view(x, i)) for i in 1:size(x, 1)]
 StatsBase.predict(o::GLM{Distributions.Poisson}, x::AVecF) = exp(dot(x, coef(o)))
 StatsBase.predict(o::GLM{Distributions.Normal}, x::AVecF) = dot(x, coef(o))
+StatsBase.predict(o::GLM{Distributions.Bernoulli}, x::AVecF) = 1.0 / (1.0 + exp(-dot(x, coef(o))))
 
 
 
 
 
 ######################### TESTING
-n, p = 1_000_000, 5
-x = randn(n, p)
-β = vcat(1.:p) / p
-y = Float64[rand(Distributions.Poisson(exp(xb))) for xb in x*β]
+if false
+    n, p = 1_000_000, 5
+    x = randn(n, p)
+    β = vcat(1.:p) / p
 
-o = OnlineStats.GLM(p, OnlineStats.LearningRate(r=.8), family = Distributions.Poisson())
-@time OnlineStats.update!(o, x, y)
-o2 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.SGD(r=.7), intercept = false)
-o3 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.ProxGrad(), intercept = false)
-o4 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.RDA(), intercept = false)
+    # POISSON
+    # y = Float64[rand(Distributions.Poisson(exp(xb))) for xb in x*β]
+    # o = OnlineStats.GLM(p, OnlineStats.LearningRate(r=.8), family = Distributions.Poisson())
+    # @time OnlineStats.update!(o, x, y)
+    # o2 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.SGD(r=.7), intercept = false)
+    # o3 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.ProxGrad(), intercept = false)
+    # o4 = OnlineStats.StochasticModel(x,y,model = OnlineStats.PoissonRegression(), algorithm = OnlineStats.RDA(), intercept = false)
 
-# y = x * β + randn(n)
-# o = OnlineStats.GLM(p, OnlineStats.LearningRate(r=.5))
-# @time OnlineStats.update!(o, x, y)
-# o2 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.SGD(), intercept = false)
-# o3 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.ProxGrad(), intercept = false)
-# o4 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.RDA(), intercept = false)
+    # BERNOULLI
+    y = Float64[rand(Distributions.Bernoulli(1 / (1 + exp(-xb)))) for xb in x*β]
+    o = OnlineStats.GLM(p, OnlineStats.LearningRate(r=.8), family = Distributions.Bernoulli())
+    @time OnlineStats.update!(o, x, y)
+    o2 = OnlineStats.StochasticModel(x,y,model = OnlineStats.LogisticRegression(), algorithm = OnlineStats.SGD(r=.7), intercept = false)
+    o3 = OnlineStats.StochasticModel(x,y,model = OnlineStats.LogisticRegression(), algorithm = OnlineStats.ProxGrad(), intercept = false)
+    o4 = OnlineStats.StochasticModel(x,y,model = OnlineStats.LogisticRegression(), algorithm = OnlineStats.RDA(), intercept = false)
 
-println("\n\n")
-println("maxabs(β - coef(o)) for")
-println()
-println("glm:      ", maxabs(β - coef(o)))
-println("sgd:      ", maxabs(β - coef(o2)))
-println("proxgrad: ", maxabs(β - coef(o3)))
-println("rda:      ", maxabs(β - coef(o4)))
+    # NORMAL
+    # y = x * β + randn(n)
+    # o = OnlineStats.GLM(p, OnlineStats.LearningRate(r=.5))
+    # @time OnlineStats.update!(o, x, y)
+    # o2 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.SGD(), intercept = false)
+    # o3 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.ProxGrad(), intercept = false)
+    # o4 = OnlineStats.StochasticModel(x, y, algorithm = OnlineStats.RDA(), intercept = false)
+
+    println("\n\n")
+    println("maxabs(β - coef(o)) for")
+    println()
+    println("glm:      ", maxabs(β - coef(o)))
+    println("sgd:      ", maxabs(β - coef(o2)))
+    println("proxgrad: ", maxabs(β - coef(o3)))
+    println("rda:      ", maxabs(β - coef(o4)))
+end #if
