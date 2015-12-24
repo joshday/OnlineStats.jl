666392b497fabbee1660667a111df3c5b28b7c06
Josh Day <emailjoshday@gmail.com>
2015-12-23 22:32:10 -0600

    normal mix fit looking good

--- src/multivariate/normalmix.jl
+++ src/multivariate/normalmix.jl
@@ -3,6 +3,9 @@ type NormalMix{W<:Weight} <: OnlineStat
     s1::VecF
     s2::VecF
     s3::VecF
+    w::VecF
+    μ::VecF
+    σ2::VecF
     weight::W
     n::Int
     nup::Int
@@ -10,9 +13,8 @@ end
 function NormalMix(k::Integer, wgt::Weight = LearningRate())
     NormalMix(
         Ds.MixtureModel(Ds.Normal[Ds.Normal(j-1, 10) for j in 1:k]),
-        zeros(k),
-        zeros(k),
-        zeros(k),
+        ones(k) / k, zeros(k), zeros(k),  # s1, s2, s3
+        zeros(k), collect(1.:k), fill(10.0, k),  # w, μ, σ2
         wgt, 0, 0
     )
 end
@@ -29,35 +31,46 @@ function Base.show(io::IO, o::NormalMix)
     printheader(io, "NormalMix (k = $(Ds.ncomponents(o)))")
     print_value_and_nobs(io, o)
 end
+function value(o::NormalMix)
+    o.value = Ds.MixtureModel(map((u,v) -> Ds.Normal(u, sqrt(v)), o.μ, o.σ2), o.s1)
+end
 
 
 function fit!(o::NormalMix, y::Real)
     γ = weight!(o, 1)
-    w = Ds.componentwise_pdf(o, y)
-    k = length(w)
-    π = Ds.probs(o)
+    k = length(o.μ)
     for j in 1:k
-        w *= π[j]
+        # o.w[j] = Ds.pdf(Ds.component(o,j), y) * o.s1[j]
+        σinv = 1 / sqrt(o.σ2[j])
+        o.w[j] = o.s1[j] * σinv * exp(-.5 * σinv * σinv * (y - o.μ[j]) ^ 2)
     end
-    w /= sum(w)
+    o.w /= sum(o.w)
+
     for j in 1:k
-        o.s1[j] = smooth(o.s1[j], w[j], γ)
-        o.s2[j] = smooth(o.s2[j], w[j] * y, γ)
-        o.s3[j] = smooth(o.s3[j], w[j] * y * y, γ)
+        o.s1[j] = smooth(o.s1[j], o.w[j], γ)
+        o.s2[j] = smooth(o.s2[j], o.w[j] * y, γ)
+        o.s3[j] = smooth(o.s3[j], o.w[j] * y * y, γ)
+
+        o.μ[j] = o.s2[j] / o.s1[j]
+        o.σ2[j] = (o.s3[j] - o.s2[j] ^ 2 / o.s1[j]) / o.s1[j]
     end
 
-    π = o.s1
-    π ./= sum(π)
-    μ = o.s2 ./ o.s1
-    σ = (o.s3 - (o.s2 .* o.s2 ./ o.s1)) ./ o.s1
-    if any(σ .<= 0.)
-        σ = ones(k)
+    ss1 = sum(o.s1)
+    for j in 1:k
+        o.s1[j] /= ss1
+        if o.σ2[j] <= 0
+            o.σ2 = ones(k)
+        end
     end
-
-    o.value = Ds.MixtureModel(map((u,v) -> Ds.Normal(u, v), vec(μ), vec(sqrt(σ))), vec(π))
 end
 
 
-y = rand(Ds.MixtureModel([Ds.Normal(0,1), Ds.Normal(5, 2), Ds.Normal(10, 10)], [.2, .4, .4]), 10000)
-@time o = NormalMix(y, 3, LearningRate(.5))
+
+# testing
+d = Ds.MixtureModel([Ds.Normal(0,1), Ds.Normal(5,2), Ds.Normal(10,10)], [.4, .2, .4])
+y = rand(d, 1_000_000)
+@time o = NormalMix(y, 3, LearningRate(.6))
+
+Profile.clear()
+@profile o = NormalMix(y, 3, LearningRate(.6))
 display(o)
