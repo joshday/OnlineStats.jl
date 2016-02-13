4e7e2a10723afcd4514fc9d18f05639d61b32be9
Josh Day <emailjoshday@gmail.com>
2016-02-13 17:12:43 -0500

    try toc

--- doc/knit.jl
+++ doc/knit.jl
@@ -3,17 +3,26 @@ module KnitDocs
 
 import OnlineStats
 
-"""
-knit(mod, dest)
-"""
+
 function knit(mod::Module, dest::AbstractString = Pkg.dir(string(mod), "doc/api.md"))
     nms = names(mod)
+    nms = setdiff(nms, [symbol(mod)])  # hack to avoid including README
     touch(dest)
     file = open(dest, "r+")
-    write(file, "# API for " * string(mod) * "\n\n")
-    print_with_color(:blue, "The following items are included in the output file:\n")
 
-    for nm in setdiff(nms, [symbol(mod)])  # hack to avoid including README
+    write(file, "# API for " * string(mod) * "\n\n")
+    write(file, "---")
+    write(file, "## Table of Contents")
+
+    for nm in nms
+        d = Docs.doc(eval(parse(string(mod) * "." * string(nm))))
+        if typeof(d) != Void
+            write(file, "* [$nm](#$nm)\n")
+        end
+    end
+
+    print_with_color(:blue, "The following items are included in the output file:\n")
+    for nm in nms
         d = Docs.doc(eval(parse(string(mod) * "." * string(nm))))
         if typeof(d) != Void
             println(nm)
