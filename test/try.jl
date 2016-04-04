72b5579e6eb414519fb2e06ae3532d58757c1c88
Josh Day <emailjoshday@gmail.com>
2016-04-03 20:18:48 -0400

    update gitignore

--- test/try.jl
+++ test/try.jl
@@ -9,14 +9,11 @@ import OnlineStats2; O2 = OnlineStats2
 n = 100_000
 y = randn(n)
 w = rand(n)
-b = 10
 
 o1 = Mean()
 o2 = O2.Mean()
 
 @time fit!(o1, y, w ./ cumsum(w))
-# @time O2.fit!(o2, y, b)
-
-# @show value(o1) - O2.value(o2)
 @show value(o1) - mean(y, WeightVec(w))
+
 end
