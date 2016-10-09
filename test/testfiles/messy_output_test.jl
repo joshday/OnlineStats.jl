5ca624e424005f7e4f8e9b056a6476c7e7f2997c
Josh Day <emailjoshday@gmail.com>
2016-10-09 11:35:13 -0400

    cleanup test files

--- test/testfiles/messy_output_test.jl
+++ test/testfiles/messy_output_test.jl
@@ -16,43 +16,11 @@ xs = hcat(x1, x)
     display(QuantileSGD(x))
     display(QuantileMM(x))
     display(Moments(x))
-    display(QuantRegMM(5))
     display(KMeans(5, 4))
     display(NormalMix(4))
     display(FitCategorical())
     display(FitBeta())
     display(BernoulliBootstrap(Mean(), mean, 1000))
-    display(StatLearn(4))
-
-    @testset "Full Factorial of Combinations" begin
-        n, p = 500, 5
-        x = randn(n, p)
-        β = collect(linspace(-1, 1, p))
-        β_with_intercept = vcat(0.0, β)
-        xβ = x*β
-        alg = [SGD(), AdaGrad(), AdaGrad2(), AdaDelta(), ADAM()] #, RDA(), MMGrad()]
-        pen = [NoPenalty(), RidgePenalty(.1), LassoPenalty(.1), ElasticNetPenalty(.1, .5)]
-        mod = [
-            LinearRegression(), L1Regression(), LogisticRegression(), DWDLike(1.0),
-            PoissonRegression(), QuantileRegression(.5), SVMLike(), HuberRegression(2.)
-        ]
-
-        generate(::LinearRegression, xβ) = xβ + randn(size(xβ, 1))
-        generate(::L1Regression, xβ) = xβ + randn(size(xβ, 1))
-        generate(::LogisticRegression, xβ) = [rand(Bernoulli(1 / (1 + exp(-η)))) for η in xβ]
-        generate(::PoissonRegression, xβ) = [rand(Poisson(exp(η))) for η in xβ]
-        generate(::QuantileRegression, xβ) = xβ + randn(size(xβ, 1))
-        generate(::SVMLike, xβ) = [rand(Bernoulli(1 / (1 + exp(-η)))) for η in xβ]
-        generate(::DWDLike, η) = generate(SVMLike(), η)
-        generate(::HuberRegression, xβ) = xβ + randn(size(xβ, 1))
-
-        for a in alg, p in pen, m in mod
-            y = generate(m, xβ)
-            println(replace("    > $a, $p, $m", "OnlineStats.", ""))
-            StatLearn(x, y, m, a, p)
-            StatLearn(x, y, 10, m, a, p)
-        end
-    end
 end
 
 end#module
