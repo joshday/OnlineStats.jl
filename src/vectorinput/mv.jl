f4ed65bfbde46a979982eee5d8548e94f0007b46
Josh Day <emailjoshday@gmail.com>
2017-03-25 19:31:08 -0400

    wip

--- src/vectorinput/mv.jl
+++ src/vectorinput/mv.jl
@@ -1,15 +1,42 @@
-#-------------------------------------------------------------------------# MV
-struct MV{T, OS <: Tuple} <: OnlineStat{VectorInput}
-    stats::OS
+# #-------------------------------------------------------------------------# MV
+# struct MV{T, OS <: Tuple} <: OnlineStat{VectorInput}
+#     stats::OS
+# end
+# function MV(args...)
+#     T = typeof(args[1])
+#     all(x -> typeof(x) == T, args) || throw(ArgumentError("arguments must be same type"))
+#     MV{T, typeof(args)}(args)
+# end
+# MV(p::Integer, o::OnlineStat{ScalarInput}) = MV([copy(o) for i in 1:p]...)
+# function Base.show{T}(io::IO, o::MV{T})
+#     s = name(o, false) * "{$T}("
+#     n = length(o.stats)
+#     for i in 1:n
+#         s *= "$(value(o.stats[i]))"
+#         if i != n
+#             s *= ", "
+#         end
+#     end
+#     s *= ")"
+#     print(io, s)
+# end
+# function fit!(o::MV, y::AVec, γ::Float64)
+#     stats = o.stats
+#     # map((stat, yi) -> fit!(stat, yi, γ), o.stats, y)
+#     for i in eachindex(y)
+#         fit!(stats[i], y[i], γ)
+#     end
+#     o
+# end
+
+
+struct MV{T} <: OnlineStat{VectorInput}
+    stats::Vector{T}
 end
-function MV(args...)
-    T = typeof(args[1])
-    all(x -> typeof(x) == T, args) || throw(ArgumentError("arguments must be same type"))
-    MV{T, typeof(args)}(args)
-end
+MV(args...) = MV(collect(args))
 MV(p::Integer, o::OnlineStat{ScalarInput}) = MV([copy(o) for i in 1:p]...)
 function Base.show{T}(io::IO, o::MV{T})
-    s = name(o, false) * "{$T}("
+    s = name(o, true) * "("
     n = length(o.stats)
     for i in 1:n
         s *= "$(value(o.stats[i]))"
@@ -22,8 +49,9 @@ function Base.show{T}(io::IO, o::MV{T})
 end
 function fit!(o::MV, y::AVec, γ::Float64)
     stats = o.stats
-    for i in eachindex(y)
-        fit!(stats[i], y[i], γ)
+    # map((stats, yi) -> fit!(stats, yi, γ), stats, y)
+    for (i, yi) in enumerate(y)
+        fit!(stats[i], yi, γ)
     end
     o
 end
