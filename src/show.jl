67b6f0ee70213164109fee8b45beb6b33052f715
Josh Day <emailjoshday@gmail.com>
2017-03-25 00:49:59 -0400

    add show.jl

---
+++ src/show.jl
@@ -0,0 +1,45 @@
+function name(o, withparams = true)
+    s = replace(string(typeof(o)), "OnlineStats.", "")
+    if !withparams
+        s = replace(s, r"\{(.*)", "")
+    end
+    s
+end
+
+header(io::IO, s::AbstractString) = print_with_color(:light_cyan, io, "■ $s")
+
+subheader(io::IO, s::AbstractString) = print_with_color(:light_cyan, io, "  ■ $s")
+
+function print_item(io::IO, name::AbstractString, value, newline=true)
+    print(io, "  >" * @sprintf("%15s", name * ": "), value)
+    newline && println(io)
+end
+
+fields_to_show(o::OnlineStat) = fieldnames(o)
+
+function printfields(io::IO, o, nms = fields_to_show(o))
+    if length(nms) != 0
+        s = "("
+        for nm in nms
+            s *= "$nm = $(getfield(o, nm))"
+            if nms[end] != nm
+                s *= ", "
+            end
+        end
+        s *= ")"
+        return print(io, s)
+    else
+        return print(io, "")
+    end
+end
+
+function Base.show(io::IO, o::OnlineStat)
+    nms = fields_to_show(o)
+    print(io, name(o))
+    print(io, "(")
+    for nm in nms
+        print(io, "$nm = $(getfield(o, nm))")
+        nm != nms[end] && print(io, ", ")
+    end
+    print(io, ")")
+end
