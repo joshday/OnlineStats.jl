263584a7b16f187e09c19680f89ba9755ba946fd
Josh Day <emailjoshday@gmail.com>
2017-10-31 14:33:40 -0400

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,14 +5,13 @@ makedocs(
     sitename = "OnlineStats.jl",
     authors = "Josh Day",
     clean = true,
-    doctest = false,
     pages = [
-        "pages/index.md",
-        "pages/weights.md",
-        "pages/series.md",
-        "pages/whatcan.md",
-        "pages/api.md",
-        "pages/newstats.md"
+        "index.md",
+        "weights.md",
+        "series.md",
+        "whatcan.md",
+        "api.md",
+        "newstats.md"
     ]
 )
 
