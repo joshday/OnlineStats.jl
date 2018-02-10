2f81901124d91d103efd8d8e6ff0c925522c5efd
Josh Day <emailjoshday@gmail.com>
2018-02-10 02:30:48 +0100

    decision tree structure

--- src/stats/decisiontree.jl
+++ src/stats/decisiontree.jl
@@ -1,209 +1,67 @@
 hoeffding_bound(R, δ, n) = sqrt(R ^ 2 * -log(δ) / 2n)
 
-
-
-mutable struct Node{T} <: ExactStat{(1, 0)}
-    nbc::NBClassifier{T}
-    decisionrule
-    left 
-    right
-    nobs::Int
-    b::Int
+#-----------------------------------------------------------------------# Node
+mutable struct Node{T}
+    stat::NBClassifier{T}
+    split::Pair{Int, Float64}  # if x[split[1]] < split[2], go left to lr[1]
+    lr::Vector{Int}  # Vector indices of left and right split
+    ind::Int
 end
+Node(p::Integer, T::Type, b::Integer) = Node(NBClassifier(p, T, b), Pair(1, -Inf), [1], 1)
 
-function Node(p::Integer, labeltype::Type)
-    Node(NBClassifier(p, labeltype), x -> 0, nothing, nothing, 0, 100)
-end
-
-function fit!(o::Node, xy::Tuple, γ::Float64)
-    o.nobs += 1
-    fit!(o.nbc, xy, γ)
-    if o.decisionrule(o) == 0
-        e = hoeffding_bound(log2(length(keys(o.nbc))), .01, o.nobs)
-        # for j in 
-    elseif o.decisionrule(o) == 1
-        fit!(o.left, xy, γ)
+function Base.show(io::IO, o::Node)
+    print_with_color(:green, io, "Node $(o.ind): ")
+    if length(o.lr) == 1
+        kys = keys(o)
+        ps = probs(o)
+        for (key, p) in zip(kys, ps)
+            print(io, key, " ($p)")
+            key != kys[end] && print(io, " ▒ ")
+        end
     else
-        fit!(o.right, xy, γ)
+        print(io, "$(o.lr[1]) or $(o.lr[2]), by x$(o.split[1]) < $(o.split[2])")
     end
 end
 
+nobs(o) = nobs(o.stat)
 
+function goto(o::Node, x::VectorOb)
+    i, v = o.split
+    ind = Int(x[i] < v) + 1
+    o.lr[ind]  # getindex 1 or 2
+end
+function goto(v::Vector{<:Node}, x::VectorOb)
+    i = 1
+    while true 
+        k = i 
+        i = goto(v[i], x)
+        if k == i 
+            return i 
+        end
+    end
+end
+
+shouldsplit(o::Node) = nobs(o) > 100000  # TODO: use Hoeffding bound
+
+Base.keys(o) = keys(o.stat)
+probs(o) = probs(o.stat)
+
+#-----------------------------------------------------------------------# DTree
 struct DTree{T} <: ExactStat{(1,0)}
-    nodes::Vector{NBClassifier{T}}
-    leaf::Vector{Bool}
-    rules::Vector
+    tree::Vector{Node{T}}
 end
-function DTree(p::Integer, T::Type)
-    DTree([NBClassifier(p, T)], [true], [(o, x) -> 1])
+function DTree(p::Integer, T::Type, b::Integer = 5)
+    DTree([Node(p, T, b)])
 end
-Base.show(io::IO, o::DTree) = print(io, "DTree of $(length(o.nodes)) leaves")
+Base.show(io::IO, o::DTree) = print(io, "DTree of $(length(o.tree)) leaves")
 
 function fit!(o::DTree, xy::Tuple, γ::Float64)
-    x, y = xy 
-    i = find_node(o, x)
-    fit!(o.nodes[i], xy, γ)
-    if nobs(o.nodes[i]) % 1000 == 0  # make split
-        for j in eachindex(x)
-            
-        end
+    x, y = xy
+    i = goto(o.tree, x)
+    node = o.tree[i]
+    stat = node.stat
+    fit!(stat, xy, γ)
+    if shouldsplit(node)
     end
 end
 
-function find_node(o::DTree, x::VectorOb)
-    isleaf = false
-    i = 1
-    while !isleaf
-        if !o.leaf[i]  # if it's not a leaf, apply next rule
-            i = o.rules[i](o, x)
-        else
-            isleaf = true
-        end
-    end
-    i
-end
-
-
-# #-----------------------------------------------------------------------# Description
-# # x1 => IndexedPartition(LabelType, SummaryType1)
-# # x2 => IndxedPartition(LabelType, SummaryType2)
-# # ...
-
-# struct Node{C, T} <: ExactStat{(1, 0)}
-#     summary::T
-#     left::C
-#     right::C
-# end
-# function Node(T::Type, summaries::OnlineStat{0}...; left=nothing, right=nothing)
-#     Node(IndexedPartition.(T, summaries), left, right)
-# end
-# function Base.show(io::IO, o::Node{C, T}) where {C, T}
-#     print(io, name(o, false, false), "($(index_type(o.summary[1])) label, ", length(o.summary), " summaries)")
-# end
-# function fit!(o::Node, xy::XyOb, γ::Float64)
-#     x, label = xy
-#     for i in eachindex(o.summary)
-#         fit!(o.summary[i], (label, x[i]), γ)
-#     end
-# end
-
-
-# struct BTree{T}
-#     dict
-#     left::T
-#     right::T
-# end
-# function fit!(o::BTree, x, y)
-#     if haskey(o.dict, y)
-#         o.dict[y]
-#     end
-# end
-
-# find_node(o::BTree{Void}) = o 
-# function find_node(o::BTree, x::VectorOb)
-#     goleft ? find_node(o.left, x) : find_node(o.right, x)
-# end
-
-
-
-
-# mutable struct VFDT{T} <: ExactStat{(1, 0)}
-#     stat_schema::T
-#     root::BTree
-# end
-# function VFDT(stat_schema)
-#     VFDT(stat_schema, BTree(Dict(), nothing, nothing))
-# end
-# Base.show(io::IO, o::VFDT) = print(io, "VFDT: $(name.(o.stat_schema, false, false))")
-# function fit!(o::VFDT, xy::XyOb, γ::Float64)
-#     x, y = xy
-#     node = find_node(o.root, x)
-# end
-
-
-
-
-
-
-# #-----------------------------------------------------------------------# OLD
-# # Review paper:
-# # https://people.cs.umass.edu/~utgoff/papers/mlj-id5r.pdf
-
-# # abstract type DecisionTreeAlgorithm end
-
-# # mutable struct Node{T, F <: Function}
-# #     counts::CountMap{T}
-# #     impurity::F
-# # end
-# # function Node(T::Type, impurity = p -> entropy(p, 2)) 
-# #     Node(CountMap(T), impurity)
-# # end
-# # function Base.show(io::IO, o::Node)
-# #     print_with_color(:green, io, "Node\n")
-# #     println(io, "  > impurity: ", o.impurity)
-# #     print(  io, "  > counts:   ", o.counts)
-# # end
-
-# # probs(o::Node) = collect(values(o)) ./ sum(values(o))
-
-
-
-# # #-----------------------------------------------------------------------# DecisionTree
-# # mutable struct DecisionTree{T <: DecisionTreeAlgorithm} <: ExactStat{(1, 0)}
-# #     alg::T
-# # end
-# # DecisionTree(args...) = DecisionTree(VFDT(args...))
-
-# # function Base.show(io::IO, o::DecisionTree)
-# #     print_with_color(:green, io, "DecisionTree: ")
-# #     print(io, o.alg)
-# # end
-
-# # fit!(o::DecisionTree, xy::Tuple{VectorOb, Any}, γ::Float64) = fit!(o.alg, xy, γ)
-
-
-# # #-----------------------------------------------------------------------# VFDT 
-# # # https://homes.cs.washington.edu/~pedrod/papers/kdd00.pdf
-
-# # # Hoeffding Tree Node
-# # mutable struct HNode{T}
-# #     ss::Vector{CountMap{T}}
-# #     children
-# # end
-# # HNode(T) = HNode(CountMap{T}[], nothing)
-
-# # function Base.show(io::IO, o::HNode)
-# #     print_with_color(:green, io, name(o))
-# #     println(io)
-# #     n_att = length(o.ss)
-# #     println(io, "  > N Attributes: ", n_att)
-# #     print(io,   "  > Children:     ", o.children)
-# # end
-
-# # struct VFDT{G, T} <: DecisionTreeAlgorithm
-# #     g::G        # split evaluation function 
-# #     δ::Float64  # 1 - δ = desired probability of choosing correct attribute to split on
-# #     tree::HNode{T}
-# # end
-# # VFDT(T, δ::Float64 = .01) = VFDT(information_gain, δ, HNode(T))
-
-# # function Base.show(io::IO, o::VFDT{G, T}) where {G, T}
-# #     print_with_color(:green, io, "VFDT")
-# #     println(io)
-# #     println(io, "  > Label Type    : ", T)
-# #     println(io, "  > Split Criteria: ", o.g)
-# #     println(io, "  > δ             : ", o.δ)
-# #     print(  io, "  > Tree Size     : ", "???")
-# # end
-
-# # # attribute selection measures (TODO)
-# # function information_gain end
-# # function gini_index end
-
-# # # helpers 
-# # 
-
-# # # assume x is a Tuple
-# # function fit!(o::VFDT, xy::Tuple{VectorOb, Any}, γ::Float64)
-
-# # end
