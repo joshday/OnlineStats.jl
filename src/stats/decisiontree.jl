eb25b982a153ebca9a77829940ce6055e5d65a25
Josh Day <emailjoshday@gmail.com>
2018-03-06 17:07:02 -0500

    tree updates

--- src/stats/decisiontree.jl
+++ src/stats/decisiontree.jl
@@ -12,7 +12,7 @@ end
 
 
 #-----------------------------------------------------------------------# NodeStats
-mutable struct NodeStats{T, O} <: OnlineStat{(1, 0)}
+mutable struct NodeStats{T, O <: OnlineStat} <: OnlineStat{(1, 0)}
     stats::Matrix{O}  # a row for each label
     labels::Vector{T}
     nobs::Vector{Int}
@@ -30,7 +30,7 @@ end
 function fit!(o::NodeStats, xy, γ)
     x, y = xy 
     addrow = true
-    for (i, lab) in enumerate(o.labels)
+    @inbounds for (i, lab) in enumerate(o.labels)
         if y == lab 
             addrow = false 
             o.nobs[i] += 1
@@ -61,25 +61,46 @@ Base.keys(o::NodeStats) = o.labels
 probs(o::NodeStats) = o.nobs ./ sum(o.nobs)
 nobs(o::NodeStats) = sum(o.nobs)
 
-#-----------------------------------------------------------------------# StumpSplit
-struct StumpSplit{T}
+function find_best_split(o::NodeStats{T}) where {T}
+    # splits = Split{T}[]
+    # nobs_left = zeros(Int, length(o.nobs))
+    # nobs_right = zeros(Int, length(o.nobs))
+
+    # for j in 1:size(o.stats, 2)
+    #     zeros!(nobs_left)
+    #     zeros!(nobs_right)
+    #     suff_stats_j = o[:, j]
+    #     for loc in split_candidates(suff_stats_j)
+    #         for (k, hk) in enumerate(suff_stats_j)
+    #             nleft = sum(hk.alg, loc)
+    #             nobs_left[k] += nleft
+    #             nobs_right[k] += nobs(hk) - nleft
+    #         end
+
+    #     end
+    # end
+end
+
+#-----------------------------------------------------------------------# Split
+struct Split{T}
     j::Int          # variable to split on
     loc::Float64    # where to split
     class::Vector{T} # class[1] = label if xj < loc, class[2] = label if xj ≥ loc
     ig::Float64     # information gain
 end
-function classify(o::StumpSplit, x::VectorOb) 
+function classify(o::Split, x::VectorOb) 
     length(o.class) == 0 && error("Call `make_split!(o)` before classifying.")
-    o.class[1 + (x[o.j] ≥ o.loc)]
+    o.class[1 + !goleft(o,x)]
 end
+goleft(o::Split, x::VectorOb) = x[o.j] < o.loc
 
 #-----------------------------------------------------------------------# Stump
-mutable struct Stump{T, O} <: ExactStat{(1, 0)}
+mutable struct Stump{T, O <: OnlineStat} <: ExactStat{(1, 0)}
     ns::NodeStats{T, O} # Sufficient statistics
-    split::StumpSplit{T}
+    split::Split{T}
 end
 function Stump(p::Int, T::Type, stat = Hist(50))
-    Stump(NodeStats(p, T, stat), StumpSplit(0, 0.0, T[], 0.0))
+    Stump(NodeStats(p, T, stat), Split(0, 0.0, T[], 0.0))
 end
 function Base.show(io::IO, o::Stump)
     println(io, "Stump")
@@ -94,15 +115,13 @@ classify(o::Stump, x::VectorOb) = classify(o.split, x)
 
 function make_split!(o::Stump{T}) where {T}
     imp_root = impurity(probs(o.ns))
-
-    nobs_left = zeros(length(o.ns.nobs))
-    nobs_right = zeros(length(o.ns.nobs))
-
-    splits = StumpSplit{T}[]
+    splits = Split{T}[]
 
     for j in 1:size(o.ns.stats, 2)
         suff_stats_j = o.ns[:, j]
         for loc in split_candidates(suff_stats_j)
+            nobs_left = zeros(length(o.ns.nobs))
+            nobs_right = zeros(length(o.ns.nobs))
             # find out nobs for each label in the left and the right
             for (k, hk) in enumerate(suff_stats_j)
                 nleft = sum(hk.alg, loc)
@@ -120,7 +139,7 @@ function make_split!(o::Stump{T}) where {T}
             # right label
             _, i = findmax(nobs_right)
             lab_right = keys(o)[i]
-            push!(splits, StumpSplit(j, loc, [lab_left, lab_right], imp_root - imp_after))
+            push!(splits, Split(j, loc, [lab_left, lab_right], imp_root - imp_after))
         end
     end
     # set the best split 
@@ -148,22 +167,20 @@ function classify(o::Stump, x::AbstractMatrix, dim::Cols)
     mapslices(x -> classify(o, x), x, 1)
 end
 
-
-
-
 #-----------------------------------------------------------------------# StumpForest 
 struct StumpForest{T <: Stump} <: ExactStat{(1, 0)}
     forest::Vector{T}
     subsets::Matrix{Int}  # subsets[:, k] gets setn to stump forest[k]
     p::Int
+    λ::Float64
 end
-function StumpForest(p::Int, T::Type; nt = 100, b = 50, np = round(Int, sqrt(p)))
+function StumpForest(p::Int, T::Type; nt = 100, b = 50, np = round(Int, sqrt(p)), λ = .1)
     forest = [Stump(np, T, Hist(b)) for _ in 1:nt] 
     subsets = zeros(Int, np, nt)
     for j in 1:nt 
         subsets[:, j] = sample(1:p, np; replace = false)
     end
-    StumpForest(forest, subsets, p)
+    StumpForest(forest, subsets, p, λ)
 end
 function Base.show(io::IO, o::StumpForest)
     println(io, name(o))
@@ -177,12 +194,10 @@ Base.keys(o::StumpForest) = keys(first(o.forest))
 nobs(o::StumpForest) = nobs(first(o.forest))
 
 function fit!(o::StumpForest, xy, γ)
-    cutoff = 2 / length(o.forest)
     x, y = xy
     for (i, stump) in enumerate(o.forest)
-        if rand() < cutoff 
-            xyi = (x[o.subsets[:, i]], y)
-            fit!(stump, xyi, γ)
+        @inbounds if rand() < o.λ 
+            @views xyi = (x[o.subsets[:, i]], y)
             fit!(stump, xyi, γ)
         end
     end
@@ -190,6 +205,7 @@ end
 
 value(o::StumpForest) = make_split!.(o.forest)
 
+# TODO: speed these up
 function predict(o::StumpForest, x::VectorOb)
     out = Dict(Pair.(keys(o), 0))
     for (i, stump) in enumerate(o.forest)
@@ -206,4 +222,49 @@ function classify(o::StumpForest, x::VectorOb)
             return first(entry)
         end
     end
+end
+
+
+#-----------------------------------------------------------------------# Node 
+struct Node{T, O <: OnlineStat} <: ExactStat{(1, 0)}
+    ns::NodeStats{T, O}
+    id::Int 
+    children::Vector{Int}
+    split::Split{T}
+end
+Base.show(io::IO, o::Node) = print(io, "Node $(o.id) with children: $(o.children)")
+shouldsplit(o::Node) = nobs(o.ns) > 10_000
+
+#-----------------------------------------------------------------------# Tree
+struct Tree{T, O} <: ExactStat{(1, 0)}
+    tree::Vector{Node{T, O}}
+    maxsize::Int
+end
+function Tree(p::Int, T::Type; b = 50, maxsize = 50) 
+    Tree([Node(NodeStats(p, T, Hist(b)), 1, Int[], Split(0, 0.0, T[], 0.0))], maxsize)
+end
+
+function Base.show(io::IO, o::Tree)
+    println(io, "Tree")
+    println(io, "    > Labels: ", keys(o))
+    print(io,   "    > Size:  ", length(o.tree))
+end
+Base.keys(o::Tree) = keys(first(o.tree).ns)
+
+function fit!(o::Tree, xy, γ)
+    x, y = xy
+    node = o.tree[whichleaf(o, x)]  # Find the node to update
+    fit!(node.ns, xy, γ)  # update sufficient statistics
+    if length(o.tree) < o.maxsize && shouldsplit(node)
+        best_split = find_best_split(node.ns)
+    end
+end
+
+function whichleaf(o::Tree, x::VectorOb)
+    i = 1
+    while length(o.tree[i].children) > 0 
+        node = o.tree[i]
+        i = node.children[1 + !goleft(node.split, x)]
+    end
+    i
 end
