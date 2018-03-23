21ba1219ca66cd8038e8273b254d5a2c57f58644
autodocs <autodocs>
2018-03-23 16:52:09 +0000

    build based on 87685c0

--- latest/search_index.js
+++ latest/search_index.js
@@ -285,7 +285,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Visualizations",
     "title": "Many Stats Can Be Plotted",
     "category": "section",
-    "text": "s = fit!(Series(Hist(25), Hist(-5:5)), randn(10^6))\nplot(s)\nsavefig(\"plot_series.png\"); nothing # hide(Image: )"
+    "text": "s = fit!(Series(Hist(25), Hist(-5:5)), randn(10^6))\nplot(s)\nsavefig(\"plot_series.png\") # hide(Image: )"
 },
 
 {
