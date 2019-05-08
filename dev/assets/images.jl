9c34ed6a25c5a9a43f78aeebf4a5f0d5a0fd0f01
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-08 03:49:58 +0000

    build based on 5d7fe8b

--- dev/assets/images.jl
+++ dev/assets/images.jl
@@ -1,76 +1,72 @@
-using OnlineStats, Plots, Colors, Random, CSV
+using OnlineStats, Plots, PlotThemes, Colors, Random, CSV
 gr()
+theme(:juno)
 Random.seed!(123)
 
-#-----------------------------------------------------------------------# readme plot
-@info "Plot 1"
-y = cumsum(randn(10^6)) + 100randn(10^6)
-o = Partition(KHist(10))
-fit!(o, y)
-p1 = plot(o)
-
-@info "Plot 2"
-p2 = plot(
-    plot(fit!(KHist(50), y[1:5_000])),
-    plot(fit!(KHist(50), y[1:200_000])),
-    plot(fit!(KHist(50), y[1:500_000])),
-    plot(fit!(KHist(50), y))
+logo_colors = hcat(
+    RGB(135/255, 222/255, 183/255),  # not in logo
+    RGB(64/255, 191/255, 169/255),
+    RGB(64/255, 169/255, 191/255),
+    RGB(64/255, 124/255, 191/255),
+    RGB(64/255, 79/255, 191/255),
 )
 
-@info "Plot 3"
-p3 = plot(fit!(PlotNN(200), randn(10^5, 2)), colorbar=false)
+# #-----------------------------------------------------------------------# readme plot
+# @info "Plot 1"
+# y = cumsum(randn(10^6)) + 100randn(10^6)
+# o = Partition(KHist(10))
+# fit!(o, y)
+# p1 = plot(o)
+
+# @info "Plot 2"
+# p2 = plot(
+#     plot(fit!(KHist(50), y[1:5_000])),
+#     plot(fit!(KHist(50), y[1:200_000])),
+#     plot(fit!(KHist(50), y[1:500_000])),
+#     plot(fit!(KHist(50), y))
+# )
+
+# @info "Plot 3"
+# p3 = plot(fit!(PlotNN(200), randn(10^5, 2)), colorbar=false)
 
 
-@info "Plot 4"
-df = CSV.read("/Users/joshday/datasets/diamonds.csv"; allowmissing=:none)
-o = Partition(CountMap(String), 75)
-p4 = plot(fit!(o, string.(df[:cut])), ylim = (0, df[1][end]))
+# @info "Plot 4"
+# df = CSV.read("/Users/joshday/datasets/diamonds.csv"; allowmissing=:none)
+# o = Partition(CountMap(String), 75)
+# p4 = plot(fit!(o, string.(df[:cut])), ylim = (0, df[1][end]))
 
-@info "Joining Plots"
-p = plot(p1, p2, p4, p3; xaxis=false, yaxis=false,xticks=[], yticks=[], size=(1000,800), legend=false)
-png("/Users/joshday/Desktop/readme.png")
-p
-
-# #-----------------------------------------------------------------------# logo
-# x = [0. 10. 20.]
-# a = @animate for i in 0:.2:30
-#     scatter(
-#         mod.(x + i, 30), zeros(1, 3), ylim = (-.5, .5), xlim = (3, 20),
-#         ms = 50,
-#         # green, red, purple
-#         color = [RGB(.22,.596,.149) RGB(.8,.361,.361) RGB(.702, .322, .8)],
-#         markerstrokewidth = 10,
-#         markerstrokecolor = [RGB(.133, .541, .133) RGB(.8, .2, .2) RGB(.584, .345, .698)],
-#         grid = false, axis = false, legend = false
-#     )
-# end
-# gif(a, @__DIR__() * "/logo.gif", fps=100)
+# @info "Joining Plots"
+# p = plot(p1, p2, p4, p3; xaxis=false, yaxis=false,xticks=[], yticks=[], size=(1000,800), legend=false)
+# png("/Users/joshday/Desktop/readme.png")
+# p
 
 #-----------------------------------------------------------------------# Weight animation
 # n = 100
 # Random.seed!(123)
 # y = .5cumsum(rand(n)) + 3randn(n)
 
-# s1 = Series(EqualWeight(),              Mean())
-# s2 = Series(ExponentialWeight(.1),      Mean())
-# s3 = Series(Bounded(EqualWeight(), .1), Mean())
-# s4 = Series(LearningRate(.6),           Mean())
-# s5 = Series(HarmonicWeight(10.),        Mean())
-# s6 = Series(McclainWeight(.1),          Mean())
+# os = (
+#     Mean(weight = EqualWeight()),
+#     Mean(weight = ExponentialWeight(.1)),
+#     Mean(weight = LearningRate(.6)),
+#     Mean(weight = HarmonicWeight(10.)),
+#     Mean(weight = McclainWeight(.1))
+# )
 
-# p = plot(zeros(0), zeros(0, 7), seriestype = [:scatter :line :line :line :line :line :line], w=1,
-#     label = ["data" "EqualWeight" "ExponentialWeight(.1)" "Bounded(EqualWeight(), .1)" "LearningRate(.6)" "HarmonicWeight(10)" "McclainWeight(.1)"], ms=2, linestyle=:auto,
+
+
+# p = plot(zeros(0), zeros(0, 6), seriestype = [:scatter :line :line :line :line :line], w=3,
+#     label = ["data" "EqualWeight" "ExponentialWeight(.1)" "LearningRate(.6)" "HarmonicWeight(10)" "McclainWeight(.1)"], ms=3,
+#     linestyle=:auto,
 #     grid=false, legend = :topleft, xlab = "Number of Observations", title="Means",
-#     palette = :darktest, ylim = (-5, 30))
+#     color = hcat(:black, logo_colors...), ylim = (-5, 30))
 
-# s = [s1, s2, s3, s4, s5, s6]
 # a = @animate for yi in y
-#     for si in s
-#         fit!(si, yi)
+#     for o in os
+#         fit!(o, yi)
 #     end
-#     vals = vcat(yi, map(x -> value(x)[1], s)...)
-#     # @show typeof(vals)
-#     push!(p, nobs(s1), vals)
+#     vals = vcat(yi, map(x -> value(x)[1], os)...)
+#     push!(p, nobs(os[1]), vals)
 # end
 
 # gif(a, @__DIR__() * "/weights.gif")
@@ -102,16 +98,15 @@ p
 
 
 #-----------------------------------------------------------------------# weight
-# p1 = plot(EqualWeight())
-# p2 = plot(ExponentialWeight(.1))
-# p3 = plot(BoundedEqualWeight(.1))
-# p4 = plot(LearningRate())
-# p5 = plot(HarmonicWeight())
-# p6 = plot(McclainWeight())
-#
-# p = plot(p1, p2, p3, p4, p5, p6, size = (1200, 800), legendfont = font(12))
-#
-# png(p, @__DIR__() * "/weights.png")
+p1 = plot(EqualWeight())
+p2 = plot(ExponentialWeight(.1))
+p4 = plot(LearningRate())
+p5 = plot(HarmonicWeight())
+p6 = plot(McclainWeight())
+
+p = plot(p1, p2, p4, p5, p6, size = (1200, 800), font = font("Bangla MN", 12), linecolor=logo_colors[2])
+
+png(p, @__DIR__() * "/weights.png")
 
 
 #-----------------------------------------------------------------------# Logo
