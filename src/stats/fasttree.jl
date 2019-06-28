b7fa765b7acf1c57bcb1a05663fe567c2fc18362
Josh Day <emailjoshday@gmail.com>
2019-06-28 12:43:23 -0400

    fix information gain tie in fasttree

--- src/stats/fasttree.jl
+++ src/stats/fasttree.jl
@@ -2,16 +2,16 @@ mutable struct FastNode{G<:Group} <: OnlineStat{XY}
     stats::Vector{G}
     ids::Vector{Int}  # self, left, right
     j::Int
-    at::Float64 
+    at::Float64
     ig::Float64
 end
 function FastNode(p, nkeys=2; stat = FitNormal(), id=1)
     FastNode([p * stat for k in 1:nkeys], [id], 0, 0.0, -Inf)
 end
-Base.show(io::IO, o::FastNode) = 
+Base.show(io::IO, o::FastNode) =
     print(io, "FastNode | $(nkeys(o)) keys × $(nvars(o)) vars | at=$((o.j, o.at))")
 
-function _fit!(o::FastNode, xy) 
+function _fit!(o::FastNode, xy)
     x, y = xy
     y in 1:nkeys(o) || error("y must be an integer in 1:nkeys")
     _fit!(o.stats[y], x)
@@ -24,7 +24,7 @@ Base.getindex(o::FastNode, i) = [stat[i] for stat in o.stats]
 
 _merge!(o::FastNode, o2::FastNode) = _merge!.(o.stats, o2.stats)
 
-function fakedata(::Type{FastNode}, n, p) 
+function fakedata(::Type{FastNode}, n, p)
     x = randn(n, p)
     y = [(rand() > 1 /(1 + exp(xb))) + 1 for xb in x * (1:p)]
     x, y
@@ -34,15 +34,18 @@ function classify(o::FastNode)
     n = nobs(o.stats[1])
     for j in 2:nkeys(o)
         n2 = nobs(o.stats[j])
-        if n2 > n 
-            out = j 
+        if n2 > n
+            out = j
             n = n2
         end
     end
     out
 end
 
-whichchild(o::FastNode, x::VectorOb) = x[o.j] < o.at ? o.ids[2] : o.ids[3]
+function whichchild(o::FastNode, x::VectorOb)
+    o.j == 0 && return rand(o.ids[2:3])
+    x[o.j] < o.at ? o.ids[2] : o.ids[3]
+end
 
 impurity(p) = entropy(p, 2)
 
@@ -53,12 +56,12 @@ function split!(o::FastNode, tree)
     nr = zeros(nkeys(o))  # "prob" right
     imp_before = impurity(probs(o))
     ig = -Inf
-    ind = 0 
+    ind = 0
     at = -Inf
     sc = zeros(9 * nkeys(o))
     for j in 1:nvars(o)
         stats_j = o[j]
-        k = 0 
+        k = 0
         for stat in stats_j # split candidates
             μ = mean(stat)
             σ = std(stat)
@@ -66,7 +69,7 @@ function split!(o::FastNode, tree)
             sc[k+2] = μ - 1.5σ
             sc[k+3] = μ - σ
             sc[k+4] = μ - .5σ
-            sc[k+5] = μ 
+            sc[k+5] = μ
             sc[k+6] = μ + .5σ
             sc[k+7] = μ + σ
             sc[k+8] = μ + 1.5σ
@@ -83,15 +86,15 @@ function split!(o::FastNode, tree)
             imp_l = impurity(nl ./ sum(nl))
             imp_r = impurity(nr ./ sum(nr))
             imp_after = smooth(imp_l, imp_r, sum(nr) / n)
-            new_ig = imp_before - imp_after 
-            if new_ig > ig 
-                ig = new_ig 
-                ind = j 
+            new_ig = imp_before - imp_after
+            if new_ig > ig
+                ig = new_ig
+                ind = j
                 at = loc
             end
         end
     end
-    o.j = ind 
+    o.j = ind
     o.at = at
     o.ig = ig
     d = length(tree)
@@ -104,7 +107,7 @@ end
 """
     FastTree(p::Int, nclasses=2; stat=FitNormal(), maxsize=5000, splitsize=1000)
 
-Calculate a decision tree of `p` predictors variables and classes `1, 2, …, nclasses`.  
+Calculate a decision tree of `p` predictors variables and classes `1, 2, …, nclasses`.
 Nodes split when they reach `splitsize` observations until `maxsize` nodes are in the tree.
 Each variable is summarized by `stat`, which can be `FitNormal()` or `Hist(nbins)`.
 
@@ -120,7 +123,7 @@ Each variable is summarized by `stat`, which can be `FitNormal()` or `Hist(nbins
 """
 struct FastTree{T<:FastNode} <: OnlineStat{XY}
     tree::Vector{T}
-    maxsize::Int 
+    maxsize::Int
     splitsize::Int
 end
 function FastTree(p, nkeys=2; stat = FitNormal(), maxsize=5000, splitsize=1000)
@@ -139,10 +142,10 @@ nkeys(o::FastTree) = nkeys(o.tree[1])
 nvars(o::FastTree) = nvars(o.tree[1])
 Base.length(o::FastTree) = length(o.tree)
 function _fit!(o::FastTree, xy)
-    x, y = xy 
+    x, y = xy
     node = whichleaf(o, x)
     _fit!(node, xy)
-    if length(o.tree) < o.maxsize && nobs(node) > o.splitsize 
+    if length(o.tree) < o.maxsize && nobs(node) > o.splitsize
         left, right = split!(node, o.tree)
         push!(o.tree, left)
         push!(o.tree, right)
@@ -158,17 +161,17 @@ function whichleaf(o, x::VectorOb)
 end
 
 classify(o::FastTree, x::VectorOb) = classify(whichleaf(o, x))
-function classify(o::FastTree, x::AbstractMatrix) 
+function classify(o::FastTree, x::AbstractMatrix)
     [classify(o, xi) for xi in OnlineStatsBase.eachrow(x)]
 end
 
-#-----------------------------------------------------------------------# FastForest 
+#-----------------------------------------------------------------------# FastForest
 """
     FastForest(p, nkeys=2; stat=FitNormal(), kw...)
 
-Calculate a random forest where each variable is summarized by `stat`.  
+Calculate a random forest where each variable is summarized by `stat`.
 
-# Keyword Arguments 
+# Keyword Arguments
 
 - `nt=100)`: Number of trees in the forest
 - `b=floor(Int, sqrt(p))`: Number of random features for each tree to receive
@@ -176,7 +179,7 @@ Calculate a random forest where each variable is summarized by `stat`.
 - `splitsize=5000`: Number of observations in any given node before splitting
 - `λ = .05`: Probability that each tree is updated on a new observation
 
-# Example 
+# Example
 
     x, y = randn(10^5, 10), rand(1:2, 10^5)
 
@@ -187,7 +190,7 @@ Calculate a random forest where each variable is summarized by `stat`.
 mutable struct FastForest{T<:FastTree} <: OnlineStat{XY}
     forest::Vector{T}
     subsets::Matrix{Int}
-    p::Int 
+    p::Int
     λ::Float64
     n::Int
 end
@@ -226,7 +229,7 @@ function _classify(o::FastForest, x::VectorOb, buffer::Vector{Int})
 end
 
 classify(o::FastForest, x::VectorOb) = _classify(o, x, zeros(Int, nkeys(o)))
-function classify(o::FastForest, x::AbstractMatrix) 
+function classify(o::FastForest, x::AbstractMatrix)
     buffer = zeros(Int, nkeys(o))
     [_classify(o, xi, buffer) for xi in OnlineStatsBase.eachrow(x)]
 end
