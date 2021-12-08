1c1279d496cfb59830b06eacb187a4fc81046255
joshday <emailjoshday@gmail.com>
2021-12-08 13:29:36 -0500

    doc string for countminsketch

--- src/stats/probabilistic.jl
+++ src/stats/probabilistic.jl
@@ -1,7 +1,30 @@
 #-----------------------------------------------------------------------------# CountMinSketch
 # https://florian.github.io/count-min-sketch/
 """
-    CountMinSketch
+    CountMinSketch(T = Number; nhash=50, hashlength=1000, storagetype=Int)
+
+Create a Count-Min Sketch with `nhash` hash functions of width `hashlength`.  Counts in the sketch 
+will be stored as `storagetype`.  
+
+This lets you calculate an upper bound on the number of occurrences of some value `x` in a data stream via
+`value(count_min_sketch, x)`.  For details and error bounds, please see the references section.
+
+# Example 
+
+    o = CountMinSketch()
+
+    y = rand(1:100, 10^6)
+
+    fit!(o, y)
+
+    value(o, 1)
+
+    sum(y .== 1)
+
+# References 
+
+- https://florian.github.io/count-min-sketch/
+
 """
 mutable struct CountMinSketch{T, IntType} <: OnlineStat{T} 
     sketch::Matrix{IntType}
